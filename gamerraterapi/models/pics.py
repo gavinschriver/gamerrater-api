c81ebcd3c8df112ef67673ce3a9368a15923cfd1
Gavin Schriver <alexanderschriver@gmail.com>
2020-11-04 11:36:08 -0600

    all fixtures setup

--- gamerraterapi/models/pics.py
+++ gamerraterapi/models/pics.py
@@ -4,6 +4,6 @@ from django.db.models.fields.files import ImageField
 
 class Pics(models.Model):
     caption = models.CharField(max_length=100)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    image = models.ImageField(upload_to=None, null=True, blank=True)
     player = models.ForeignKey('Player', on_delete=CASCADE)
     game = models.ForeignKey('Game', on_delete=CASCADE)
