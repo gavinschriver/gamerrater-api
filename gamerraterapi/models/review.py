aaef76874c2b9fdf7889ea270e2472c90d9c1897
Gavin Schriver <alexanderschriver@gmail.com>
2020-11-07 17:34:16 -0600

    changing models/fixtures for ints on game time to play, auto-now false on review timestamps

--- gamerraterapi/models/review.py
+++ gamerraterapi/models/review.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Review(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     text = models.CharField(max_length=100)
     player = models.ForeignKey('Player', on_delete=models.CASCADE)
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
